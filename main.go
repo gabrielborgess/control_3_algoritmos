976738b52d111b82b8581ccd8179f39cdb527d86
gabriel <gbordesc@uft.edu>
2020-12-02 15:03:46 -0300

    Repaso de lsitas

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import(
+	"fmt"
+	)
+
+
+func main(){
+	var lista []int
+
+	lista = append(lista,1)
+	fmt.Println(lista)
+	fmt.Println(lista[0])
+
+
+}
+
+////
+//func desglosar(n int,lista[]int){
+//
+//}
