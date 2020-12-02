03a7ddf82f6295f63dbc45d81e149252d8150322
gabriel <gbordesc@uft.edu>
2020-12-02 15:24:28 -0300

    Agregado full recursion

--- main.go
+++ main.go
@@ -7,15 +7,28 @@ import(
 
 func main(){
 	var lista []int
+	lista = desglosar(9,lista)//Se desglosa la lista
+	lista=invertir(lista)//Se invierte la lista desglosada
 
-	lista = append(lista,1)
 	fmt.Println(lista)
-	fmt.Println(lista[0])
-
 
 }
 
-////
-//func desglosar(n int,lista[]int){
-//
-//}
+
+func desglosar(n int,lista[]int) []int{
+
+	if n==0{
+		return lista
+	}else{
+		lista=append(lista,n)
+		return desglosar(n-1,lista)
+	}
+}
+
+
+func invertir(lista []int) []int {
+	if len(lista) == 0 {
+		return lista
+	}
+	return append(invertir(lista[1:]), lista[0])
+}
